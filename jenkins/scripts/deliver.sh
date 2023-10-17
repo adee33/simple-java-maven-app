c8ff0a4e7987c9a98acefe76db2342fc35cd80e3
adee33 <adeonggo@hotmail.com>
2023-10-17 13:44:24 +0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -17,10 +17,10 @@ set +x
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION="1.0-SNAPSHOT"
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/my-app-1.0-SNAPSHOT.jar
+java -jar target/${NAME}-${VERSION}.jar
